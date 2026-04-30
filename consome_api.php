bef4201bb6e8ca751d8ebc7b3183e54e3926b35e
mariana438 <marianare438@gmail.com>
2026-04-30 20:48:36 -0300

    Implement API consumption and display products

--- consome_api.php
+++ consome_api.php
@@ -1,1 +1,11 @@
-
+<?php
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "http://localhost/api.php");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+curl_close($ch);
+$produtos = json_decode($response, true);
+foreach ($produtos as $produto) {
+    echo "ID: " . $produto['id'] . " - Nome: " . $produto['nome'] . " - Preço: " . $produto['preco'] . "<br>";
+}
+?>
