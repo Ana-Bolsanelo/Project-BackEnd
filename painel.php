3cc063935bf8b95dbb676c142113d6d2468a8309
mariana438 <marianare438@gmail.com>
2026-04-30 20:29:21 -0300

    Update painel.php

---
+++ painel.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+if (!isset($_SESSION['usuario'])) {
+    header("Location: login.php");
+    exit();
+}
+echo "Bem-vindo, ". $SESSION['usuario'];
+?>
+<a href="logout.php">Sair</a>
