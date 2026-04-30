e99dfa6df06591acd37a91c88c8a032b84236f5f
Ana <anacbolsaneloj@gmail.com>
2026-04-30 20:25:21 -0300

    login.php

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
-    <form action="processa_login.php" method="POST">
-        <label>Email:</label>
-        <input type="email" name="email" required>
-        <label>Senha:</label>label>
-        <input type="password" name="senha" required>
-        <button type="submit">Entrar</button>
-    </form>
+<form action="processa_login.php" method="POST">
+    <label>Email:</label>
+    <input type="email" name="email" required>
+    <label>Senha:</label>label>
+    <input type="password" name="senha" required>
+    <button type="submit">Entrar</button>
+</form>
