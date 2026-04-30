005928ef249224d4a89341f8565336cdee81730e
gabriella bertoldi <gabiazevedobrt@gmail.com>
2026-04-30 20:38:28 -0300

    gabs created recuperar_senha.php

---
+++ recuperar_senha.php
@@ -0,0 +1,5 @@
+<form action="processa_recuperacao.php" methog="POST">
+  <label> Email cadastrado: </label>
+  <input type="email" name="email" required>
+  <button type="submit"> Recuperar Senha </button>
+</form>
