020badb91c78dbbd3de1a4687809f1201a41051e
mariana438 <marianare438@gmail.com>
2026-04-30 21:34:49 -0300

    Update get_produtos.php

--- get_produtos.php
+++ get_produtos.php
@@ -4,13 +4,13 @@ header('Access-Control-Allow-Origin: "');
 include 'conexao.php';
 
 switch ($_SERVER['REQUEST_METHOD']) {
-  case 'GET':
-      // Buscar todos os produtos
-      $sql = "SELECT = FROM produtos";
-      $result = $conn->query($sql);
-      $produtos = [];
-      while ($row = $result->fetch_assoc()) {
-          $produtos[] = $row;
-      )
-      echo json_encode($produtos);
-      break;
+    case 'GET':
+        // Buscar todos os produtos
+        $sql = "SELECT = FROM produtos";
+        $result = $conn->query($sql);
+        $produtos = [];
+        while ($row = $result->fetch_assoc()) {
+            $produtos[] = $row;
+        )
+        echo json_encode($produtos);
+        break;
