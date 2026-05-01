04196e1c7cc710d379fa7e8e9633c65a6584ddc1
mariana438 <marianare438@gmail.com>
2026-04-30 21:33:59 -0300

    Update post_produtos.php

--- post_produtos.php
+++ post_produtos.php
@@ -1,12 +1,12 @@
 case 'POST':
-  // Criar novo produto
-  $dados = json_decode(file_get_contents('php://input'), true);
-  $nome = $dados['nome'];
-  $descricao = $dados['descricao'];
-  $preco = $dados['preco'];
-  $sql = "INSERT INTO produtos (nome, descricao, preco) VALUES ('$nome', '$descricao', '$preco')";
-  if ($conn->query($sql) === TRUE) {
-      echo json_encode(['message' => 'Produto criado com sucesso!']);
-  } else {
-      echo json_encode(['error' => 'Erro ao criar produto.']);
-  break;
+    // Criar novo produto
+    $dados = json_decode(file_get_contents('php://input'), true);
+    $nome = $dados['nome'];
+    $descricao = $dados['descricao'];
+    $preco = $dados['preco'];
+    $sql = "INSERT INTO produtos (nome, descricao, preco) VALUES ('$nome', '$descricao', '$preco')";
+    if ($conn->query($sql) === TRUE) {
+        echo json_encode(['message' => 'Produto criado com sucesso!']);
+    } else {
+        echo json_encode(['error' => 'Erro ao criar produto.']);
+    break;
