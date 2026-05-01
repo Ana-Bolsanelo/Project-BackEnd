84799d905905a67ac6fd1f9b719e6e2ba552cf6a
mariana438 <marianare438@gmail.com>
2026-04-30 21:33:19 -0300

    Update and rename put_produtos_{id}.php to put_produtos{id}.php

---
+++ put_produtos{id}.php
@@ -0,0 +1,14 @@
+case 'PUT':
+    // Atualizar produto
+    $dados = json_decode(file_get_contents('php://input'), true);
+    $id = $_GET['id'];
+    $nome = $dados['nome'];
+    $descricao = $dados['descricao'];
+    $preco = $dados['preco'];
+    $sql = "UPDATE produtos SET nome='$nome', descricao='$descricao', preco='$preco' WHERE id=$id";
+    if ($conn->query($sql) === TRUE) {
+        echo json_encode(['message' => 'Produto atualizado com sucesso!']);
+    } else {
+        echo json_encode(['error' => 'Erro ao atualizar produto.']);
+    }
+    break;
